6def28f9506c179d7596a784139429dbbba76c6d
Gangbros777 <161280210+Gangbros777@users.noreply.github.com>
2024-06-14 23:34:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const Biography = {};
+
+
+Biography.name = prompt("Type your name: ");
+
+
+Biography.password = +prompt("Enter your password: ");
+
+
+Biography.login = +prompt("Login: ");
+
+console.log(Biography);
